4e95f98a909422b565fdf4b77b754b5b5f81c768
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-01-16 17:52:38 +0100

    Testing with canceled context

--- backend/consul/consul_test.go
+++ backend/consul/consul_test.go
@@ -10,10 +10,13 @@ import (
 )
 
 func TestConsulBackend(t *testing.T) {
+	prefix := "confita-tests"
+
 	client, err := api.NewClient(api.DefaultConfig())
 	require.NoError(t, err)
+	defer client.KV().DeleteTree(prefix, nil)
 
-	b := NewBackend(client, "prefix")
+	b := NewBackend(client, prefix)
 
 	t.Run("NotFound", func(t *testing.T) {
 		_, err = b.Get(context.Background(), "something that doesn't exist")
@@ -21,12 +24,21 @@ func TestConsulBackend(t *testing.T) {
 	})
 
 	t.Run("OK", func(t *testing.T) {
-		_, err = client.KV().Put(&api.KVPair{Key: "prefix/key1", Value: []byte("value")}, nil)
+		_, err = client.KV().Put(&api.KVPair{Key: prefix + "/key1", Value: []byte("value")}, nil)
 		require.NoError(t, err)
-		defer client.KV().Delete("prefix/key1", nil)
 
 		val, err := b.Get(context.Background(), "key1")
 		require.NoError(t, err)
 		require.Equal(t, []byte("value"), val)
+	})
+
+	t.Run("Canceled", func(t *testing.T) {
+		_, err = client.KV().Put(&api.KVPair{Key: prefix + "/key2", Value: []byte("value")}, nil)
+		require.NoError(t, err)
+
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+		_, err = b.Get(ctx, "key2")
+		require.Error(t, err)
 	})
 }
