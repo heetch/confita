45dba7a695b2ae13c4ade4a1df1d12ffb84ab3d5
Asdine El Hrychy <asdine.elhrychy@gmail.com>
2018-04-19 14:49:02 +0200

    Add Prefetch option to Consul backend

--- backend/consul/consul.go
+++ backend/consul/consul.go
@@ -3,6 +3,7 @@ package consul
 import (
 	"context"
 	"path"
+	"strings"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/heetch/confita/backend"
@@ -10,20 +11,38 @@ import (
 
 // Backend loads keys from Consul.
 type Backend struct {
-	client *api.Client
-	prefix string
+	client   *api.Client
+	prefix   string
+	prefetch bool
+	cache    map[string][]byte
 }
 
 // NewBackend creates a configuration loader that loads from Consul.
-func NewBackend(client *api.Client, prefix string) *Backend {
-	return &Backend{
+func NewBackend(client *api.Client, opts ...Option) *Backend {
+	b := Backend{
 		client: client,
-		prefix: prefix,
 	}
+
+	for _, opt := range opts {
+		opt(&b)
+	}
+
+	return &b
 }
 
 // Get loads the given key from Consul.
 func (b *Backend) Get(ctx context.Context, key string) ([]byte, error) {
+	if b.cache == nil && b.prefetch {
+		err := b.fetchTree(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if b.cache != nil {
+		return b.fromCache(ctx, key)
+	}
+
 	var opt api.QueryOptions
 
 	kv, _, err := b.client.KV().Get(path.Join(b.prefix, key), opt.WithContext(ctx))
@@ -38,7 +57,52 @@ func (b *Backend) Get(ctx context.Context, key string) ([]byte, error) {
 	return kv.Value, nil
 }
 
+func (b *Backend) fetchTree(ctx context.Context) error {
+	var opt api.QueryOptions
+
+	list, _, err := b.client.KV().List(b.prefix, opt.WithContext(ctx))
+	if err != nil {
+		return err
+	}
+
+	b.cache = make(map[string][]byte)
+
+	for _, kv := range list {
+		b.cache[strings.TrimLeft(kv.Key, b.prefix+"/")] = kv.Value
+	}
+
+	return nil
+}
+
+func (b *Backend) fromCache(ctx context.Context, key string) ([]byte, error) {
+	v, ok := b.cache[key]
+	if !ok {
+		return nil, backend.ErrNotFound
+	}
+
+	return v, nil
+}
+
 // Name returns the name of the backend.
 func (b *Backend) Name() string {
 	return "consul"
+}
+
+// Option is used to configure the Consul backend.
+type Option func(*Backend)
+
+// WithPrefix is used to specify the prefix to prepend on every keys.
+func WithPrefix(prefix string) Option {
+	return func(b *Backend) {
+		b.prefix = prefix
+	}
+}
+
+// WithPrefetch is used to prefetch the entire tree and cache it to
+// avoid further round trips. If the WithPrefix option is used, will fetch
+// the tree under the specified prefix.
+func WithPrefetch() Option {
+	return func(b *Backend) {
+		b.prefetch = true
+	}
 }
