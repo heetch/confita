48128aabffb8ba7408128966c66c544e802104b8
karantin2020 <karantin3030@mail.ru>
2018-07-28 10:21:05 +0500

    Added option funcs to env backend

--- backend/env/env.go
+++ backend/env/env.go
@@ -11,20 +11,35 @@ import (
 // NewBackend creates a configuration loader that loads from the environment.
 // If the key is not found, this backend tries again by turning any kebabcase key to snakecase and
 // lowercase letters to uppercase.
-func NewBackend() backend.Backend {
+func NewBackend(fns ...opt) backend.Backend {
 	return backend.Func("env", func(ctx context.Context, key string) ([]byte, error) {
-		val, ok := os.LookupEnv(key)
+		val, ok := os.LookupEnv(opts(key, fns...))
 		if ok {
 			return []byte(val), nil
 		}
-
-		key = strings.Replace(strings.ToUpper(key), "-", "_", -1)
-
-		val, ok = os.LookupEnv(key)
-		if ok {
-			return []byte(val), nil
-		}
-
 		return nil, backend.ErrNotFound
 	})
+}
+
+// WithPreffix adds preffix for searching env variable
+func WithPreffix(preffix string) opt {
+	return func(key string) string {
+		return preffix + key
+	}
+}
+
+// ToUpper uppercases searching env variable
+func ToUpper() opt {
+	return func(key string) string {
+		return strings.Replace(strings.ToUpper(key), "-", "_", -1)
+	}
+}
+
+type opt func(key string) string
+
+func opts(key string, fns ...opt) string {
+	for i := range fns {
+		key = fns[i](key)
+	}
+	return key
 }
