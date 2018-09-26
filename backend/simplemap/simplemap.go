e5c9c40c865321434a32cbc6ab4079b8c758f7a6
Marcin Podlodowski <marcin.podlodowski@symatic.io>
2018-09-26 02:13:08 +0200

    simplemap StructLoader approach

--- backend/simplemap/simplemap.go
+++ backend/simplemap/simplemap.go
@@ -1,16 +1,16 @@
 package simplemap
 
 import (
-	"time"
-	"fmt"
-	"strconv"
-	"github.com/heetch/confita/backend"
+	"github.com/heetch/confita"
+	"github.com/pkg/errors"
+	"reflect"
+
 	"context"
 )
 
 // Backend that loads config from map
-type Backend struct{
-	theMap	map[string]interface{}
+type Backend struct {
+	theMap map[string]interface{}
 }
 
 // NewBackend creates a simplemap backend.
@@ -20,34 +20,22 @@ func NewBackend(theMap map[string]interface{}) *Backend {
 	}
 }
 
-// Get loads the given key from the map
+// LoadStruct takes a struct config and loads the map into it
+func (b *Backend) LoadStruct(ctx context.Context, cfg *confita.StructConfig) error {
+	for _, f := range cfg.Fields {
+		mapVal := b.theMap[f.Key]
+		if mapVal == nil {
+			continue
+		}
+		mapRef := reflect.ValueOf(mapVal)
+		f.Value.Set(mapRef)
+	}
+	return nil
+}
+
+// Get is not implemented.
 func (b *Backend) Get(ctx context.Context, key string) ([]byte, error) {
-	val := b.theMap[key]
-	if val == nil {
-		return nil, backend.ErrNotFound
-	}
-
-	if str, ok := val.(string); ok {
-		return []byte(str), nil
-	}
-
-	if u, ok := val.(uint); ok {
-		return []byte(strconv.FormatUint(uint64(u), 10)), nil
-	}
-
-	if i, ok := val.(int); ok {
-		return []byte(strconv.FormatInt(int64(i), 10)), nil
-	}
-
-	if f, ok := val.(float64); ok {
-		return []byte(fmt.Sprintf("%g", f)), nil
-	}
-
-	if d, ok := val.(time.Duration); ok {
-		return []byte(fmt.Sprintf("%s", d)), nil
-	}
-
-	return nil, backend.ErrNotFound
+	return nil, errors.New("not implemented")
 }
 
 // Name returns the name of the flags backend.
--- backend/simplemap/simplemap_test.go
+++ backend/simplemap/simplemap_test.go
@@ -1,54 +1,39 @@
 package simplemap_test
 
 import (
-	"github.com/heetch/confita/backend"
-	"github.com/stretchr/testify/assert"
 	"context"
+	"github.com/heetch/confita"
 	"github.com/heetch/confita/backend/simplemap"
-	"time"
-	"testing"
 	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
 )
 
 func TestSimplemapBackend(t *testing.T) {
 	type config struct {
-		Name    string
-		Age     int
-		Balance float64
-		Timeout time.Duration
+		Name     string        `config:"Name"`
+		Age      int           `config:"Age"`
+		Negative int           `config:"Negative"`
+		Balance  float64       `config:"Balance"`
+		Timeout  time.Duration `config:"Timeout"`
 	}
 
 	b := simplemap.NewBackend(map[string]interface{}{
-		"Name": "Bob",
-		"Age": uint(30),
+		"Name":     "Bob",
+		"Age":      30,
 		"Negative": -10,
-		"Balance": 1234.56,
-		"Timeout": 10 * time.Second,
+		"Balance":  1234.56,
+		"Timeout":  10 * time.Second,
 	})
-	
-	assert.Equal(t, "simplemap", b.Name())
 
-	val, err := b.Get(context.Background(), "Name")
+	var c config
+	require.Equal(t, "simplemap", b.Name())
+	err := confita.NewLoader(b).Load(context.Background(), &c)
+
 	require.NoError(t, err)
-	assert.Equal(t, "Bob", string(val))
-
-	val, err = b.Get(context.Background(), "Age")
-	require.NoError(t, err)
-	assert.Equal(t, "30", string(val))
-
-	val, err = b.Get(context.Background(), "Negative")
-	require.NoError(t, err)
-	assert.Equal(t, "-10", string(val))
-
-	val, err = b.Get(context.Background(), "Balance")
-	require.NoError(t, err)
-	assert.Equal(t, "1234.56", string(val))
-
-	val, err = b.Get(context.Background(), "Timeout")
-	require.NoError(t, err)
-	assert.Equal(t, "10s", string(val))
-
-	_, err = b.Get(context.Background(), "NotExists")
-	require.Error(t, err)
-	assert.Equal(t, backend.ErrNotFound, err)
+	require.Equal(t, "Bob", c.Name)
+	require.Equal(t, 30, c.Age)
+	require.Equal(t, -10, c.Negative)
+	require.Equal(t, 1234.56, c.Balance)
+	require.Equal(t, 10*time.Second, c.Timeout)
 }
