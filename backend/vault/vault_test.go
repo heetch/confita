06d1be3acaa4ad456a368d6d438d9351c29da619
Philipp Gillé <philipp@heetch.com>
2021-03-20 18:38:48 +0100

    Add support for Vault KV secrets engine v2

--- backend/vault/vault_test.go
+++ backend/vault/vault_test.go
@@ -6,9 +6,10 @@ import (
 	"testing"
 
 	"github.com/hashicorp/vault/api"
-	"github.com/heetch/confita/backend"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/heetch/confita/backend"
 )
 
 func TestVaultBackend(t *testing.T) {
@@ -34,8 +35,54 @@ func TestVaultBackend(t *testing.T) {
 
 		_, err = c.Write(path,
 			map[string]interface{}{
-				"foo":    "bar",
-				"cheese": "nan",
+				"foo":  "bar",
+				"data": "nan",
+			})
+		require.NoError(t, err)
+
+		val, err := b.Get(context.Background(), "foo")
+		require.NoError(t, err)
+		assert.Equal(t, "bar", string(val))
+
+		val, err = b.Get(context.Background(), "data")
+		require.NoError(t, err)
+		assert.Equal(t, "nan", string(val))
+	})
+
+	t.Run("NotFound", func(t *testing.T) {
+		b := NewBackend(c, path)
+		_, err := b.Get(context.Background(), "badKey")
+		require.EqualError(t, err, backend.ErrNotFound.Error())
+	})
+}
+
+func TestVaultBackendV2(t *testing.T) {
+	os.Setenv("VAULT_ADDR", "http://127.0.0.1:8222")
+	client, err := api.NewClient(api.DefaultConfig())
+	require.NoError(t, err)
+
+	client.SetToken("root")
+	c := client.Logical()
+
+	path := "secret/data/test"
+
+	defer c.Delete(path)
+
+	t.Run("SecretPathNotFound", func(t *testing.T) {
+		b := NewBackendV2(c, path)
+		_, err := b.Get(context.Background(), "foo")
+		require.EqualError(t, err, "secret not found at the following path: secret/data/test")
+	})
+
+	t.Run("OK", func(t *testing.T) {
+		b := NewBackendV2(c, path)
+
+		_, err = c.Write(path,
+			map[string]interface{}{
+				"data": map[string]string{
+					"foo":    "bar",
+					"cheese": "nan",
+				},
 			})
 		require.NoError(t, err)
 
@@ -49,7 +96,7 @@ func TestVaultBackend(t *testing.T) {
 	})
 
 	t.Run("NotFound", func(t *testing.T) {
-		b := NewBackend(c, path)
+		b := NewBackendV2(c, path)
 		_, err := b.Get(context.Background(), "badKey")
 		require.EqualError(t, err, backend.ErrNotFound.Error())
 	})
