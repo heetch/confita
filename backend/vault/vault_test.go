a80b7484f23144a02a322103dcfb1721a081fd36
Philipp Gillé <philippgille@users.noreply.github.com>
2021-03-24 23:02:38 +0100

    Add support for Vault KV secrets engine v2 (#86)
    * Fix vault couldn't get a value
    
    * Add support for Vault KV secrets engine v2
    
    * Allow simpler paths like the Vault CLI does
    
    * Fix Vault tests fail when run twice
    
    Due to data versioning the deferred deletion doesn't completely remove
    the secret - only the data.
    
    * Improve path change
    
    Co-authored-by: Jean-Philippe Moal <skateinmars@users.noreply.github.com>
    
    Co-authored-by: mopemope <yutaka.matsubara@gmail.com>
    Co-authored-by: Jean-Philippe Moal <skateinmars@users.noreply.github.com>

--- backend/vault/vault_test.go
+++ backend/vault/vault_test.go
@@ -2,15 +2,23 @@ package vault
 
 import (
 	"context"
+	"math/rand"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/vault/api"
-	"github.com/heetch/confita/backend"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/heetch/confita/backend"
 )
 
+const letterBytes = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 func TestVaultBackend(t *testing.T) {
 	os.Setenv("VAULT_ADDR", "http://127.0.0.1:8200")
 	client, err := api.NewClient(api.DefaultConfig())
@@ -34,8 +42,8 @@ func TestVaultBackend(t *testing.T) {
 
 		_, err = c.Write(path,
 			map[string]interface{}{
-				"foo":    "bar",
-				"cheese": "nan",
+				"foo":  "bar",
+				"data": "nan",
 			})
 		require.NoError(t, err)
 
@@ -43,7 +51,7 @@ func TestVaultBackend(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, "bar", string(val))
 
-		val, err = b.Get(context.Background(), "cheese")
+		val, err = b.Get(context.Background(), "data")
 		require.NoError(t, err)
 		assert.Equal(t, "nan", string(val))
 	})
@@ -53,4 +61,77 @@ func TestVaultBackend(t *testing.T) {
 		_, err := b.Get(context.Background(), "badKey")
 		require.EqualError(t, err, backend.ErrNotFound.Error())
 	})
+}
+
+func TestVaultBackendV2(t *testing.T) {
+	os.Setenv("VAULT_ADDR", "http://127.0.0.1:8222")
+	client, err := api.NewClient(api.DefaultConfig())
+	require.NoError(t, err)
+
+	client.SetToken("root")
+	c := client.Logical()
+
+	randPathSuffix := randStringBytes(5)
+	path := "secret/data/" + randPathSuffix
+
+	defer c.Delete(path)
+
+	t.Run("SecretPathNotFound", func(t *testing.T) {
+		b := NewBackendV2(c, path)
+		_, err := b.Get(context.Background(), "foo")
+		require.EqualError(t, err, "secret not found at the following path: "+path)
+	})
+
+	okTests := []struct {
+		name string
+		path string
+	}{
+		{
+			"OK v2 data path",
+			path,
+		},
+		{
+			"OK old path",
+			"secret/" + randPathSuffix,
+		},
+	}
+	for _, okTest := range okTests {
+		t.Run(okTest.name, func(t *testing.T) {
+			b := NewBackendV2(c, okTest.path)
+
+			// For writing we use the Consul client directly,
+			// so we need to use the full proper path.
+			_, err = c.Write(path,
+				map[string]interface{}{
+					"data": map[string]string{
+						"foo":  "bar",
+						"data": "nan",
+					},
+				})
+			require.NoError(t, err)
+
+			val, err := b.Get(context.Background(), "foo")
+			require.NoError(t, err)
+			assert.Equal(t, "bar", string(val))
+
+			val, err = b.Get(context.Background(), "data")
+			require.NoError(t, err)
+			assert.Equal(t, "nan", string(val))
+		})
+	}
+
+	t.Run("NotFound", func(t *testing.T) {
+		b := NewBackendV2(c, path)
+		_, err := b.Get(context.Background(), "badKey")
+		require.EqualError(t, err, backend.ErrNotFound.Error())
+	})
+
+}
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
 }
