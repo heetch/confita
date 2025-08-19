434abbc0bba527ac624bd186d69f60fa5e4c22d9
Philipp Gillé <philipp@heetch.com>
2025-08-19 22:03:06 +0200

    Extend flags unit tests
    Skip some time pointers, TBD

--- backend/flags/flags_test.go
+++ backend/flags/flags_test.go
@@ -7,9 +7,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/heetch/confita"
 	"github.com/heetch/confita/backend"
-	"github.com/stretchr/testify/require"
 )
 
 func runHelper(t *testing.T, cfg any, args ...string) {
@@ -30,9 +31,12 @@ func TestFlags(t *testing.T) {
 			D int           `config:"d"`
 			E uint          `config:"e"`
 			F float32       `config:"f"`
+			G time.Time     `config:"g"`
+			H *time.Time    `config:"h"`
 		}
 		var cfg config
-		runHelper(t, &cfg, "-a=hello", "-b=true", "-c=10s", "-d=-100", "-e=1", "-f=100.01")
+		runHelper(t, &cfg, "-a=hello", "-b=true", "-c=10s", "-d=-100", "-e=1", "-f=100.01", "-g=2001-01-01T01:01:01Z", "-h=2001-01-01T01:01:01Z")
+		expTime := time.Date(2001, 1, 1, 1, 1, 1, 0, time.UTC)
 		require.Equal(t, config{
 			A: "hello",
 			B: true,
@@ -40,6 +44,8 @@ func TestFlags(t *testing.T) {
 			D: -100,
 			E: 1,
 			F: 100.01,
+			G: expTime,
+			H: &expTime,
 		}, cfg)
 	})
 
@@ -51,15 +57,22 @@ func TestFlags(t *testing.T) {
 			Ddef int           `config:"d-def,short=dd"`
 			Edef uint          `config:"e-def,short=ed"`
 			Fdef float32       `config:"f-def,short=fd"`
+			Gdef time.Time     `config:"g-def,short=gd"`
+			// Hdef *time.Time    `config:"h-def,short=hd"`
 		}
+		defaultTime := time.Date(2000, 0, 0, 0, 0, 0, 0, time.UTC)
 		cfg := &config{
 			Adef: "hello",
 			Bdef: true,
 			Cdef: 10 * time.Second,
 			Ddef: -100,
+			Edef: 200,
+			Fdef: 1.23,
+			Gdef: defaultTime,
+			// Hdef: &defaultTime,
 		}
-		runHelper(t, cfg, "-a-def=bye", "-b-def=false", "-c-def=15s", "-d-def=-200", "-e-def=400", "-f-def=2.33")
-
+		runHelper(t, cfg, "-a-def=bye", "-b-def=false", "-c-def=15s", "-d-def=-200", "-e-def=400", "-f-def=2.33", "-g-def=2001-01-01T01:01:01Z" /*, "-h-def=2001-01-01T01:01:01Z"*/)
+		expTime := time.Date(2001, 1, 1, 1, 1, 1, 0, time.UTC)
 		require.Equal(t, &config{
 			Adef: "bye",
 			Bdef: false,
@@ -67,6 +80,8 @@ func TestFlags(t *testing.T) {
 			Ddef: -200,
 			Edef: 400,
 			Fdef: 2.33,
+			Gdef: expTime,
+			// Hdef: &expTime,
 		}, cfg)
 	})
 }
@@ -79,14 +94,22 @@ func TestFlagsShort(t *testing.T) {
 		Ddef int           `config:"d-def,short=dd"`
 		Edef uint          `config:"e-def,short=ed"`
 		Fdef float32       `config:"f-def,short=fd"`
+		Gdef time.Time     `config:"g-def,short=gd"`
+		// Hdef *time.Time    `config:"h-def,short=hd"`
 	}
+	defaultTime := time.Date(2000, 0, 0, 0, 0, 0, 0, time.UTC)
 	cfg := &config{
 		Adef: "hello",
 		Bdef: true,
 		Cdef: 10 * time.Second,
 		Ddef: -100,
+		Edef: 200,
+		Fdef: 1.23,
+		Gdef: defaultTime,
+		// Hdef: &defaultTime,
 	}
-	runHelper(t, cfg, "-ad=hello", "-bd=true", "-cd=20s", "-dd=500", "-ed=700", "-fd=333.33")
+	runHelper(t, cfg, "-ad=hello", "-bd=true", "-cd=20s", "-dd=500", "-ed=700", "-fd=333.33", "-gd=2001-01-01T01:01:01Z" /*"-hd=2001-01-01T01:01:01Z"*/)
+	expTime := time.Date(2001, 1, 1, 1, 1, 1, 0, time.UTC)
 	require.Equal(t, &config{
 		Adef: "hello",
 		Bdef: true,
@@ -94,6 +117,8 @@ func TestFlagsShort(t *testing.T) {
 		Ddef: 500,
 		Edef: 700,
 		Fdef: 333.33,
+		Gdef: expTime,
+		// Hdef: &expTime,
 	}, cfg)
 }
 
@@ -105,14 +130,22 @@ func TestFlagsMixed(t *testing.T) {
 		Ddef int           `config:"d-def,short=dd"`
 		Edef uint          `config:"e-def,short=ed"`
 		Fdef float32       `config:"f-def,short=fd"`
+		Gdef time.Time     `config:"g-def,short=gd"`
+		// Hdef *time.Time    `config:"h-def,short=hd"`
 	}
+	defaultTime := time.Date(2000, 0, 0, 0, 0, 0, 0, time.UTC)
 	cfg := &config{
 		Adef: "hello",
 		Bdef: true,
 		Cdef: 10 * time.Second,
 		Ddef: -100,
+		Edef: 200,
+		Fdef: 1.23,
+		Gdef: defaultTime,
+		// Hdef: &defaultTime,
 	}
-	runHelper(t, cfg, "-ad=hello", "-b-def=true", "-cd=20s", "-d-def=500", "-ed=600", "-f-def=42.42")
+	runHelper(t, cfg, "-ad=hello", "-b-def=true", "-cd=20s", "-d-def=500", "-ed=600", "-f-def=42.42", "-gd=2001-01-01T01:01:01Z" /*"-hd=2001-01-01T01:01:01Z"*/)
+	expTime := time.Date(2001, 1, 1, 1, 1, 1, 0, time.UTC)
 	require.Equal(t, &config{
 		Adef: "hello",
 		Bdef: true,
@@ -120,6 +153,8 @@ func TestFlagsMixed(t *testing.T) {
 		Ddef: 500,
 		Edef: 600,
 		Fdef: 42.42,
+		Gdef: expTime,
+		// Hdef: &expTime,
 	}, cfg)
 }
 
@@ -131,6 +166,8 @@ func TestWithAnotherBackend(t *testing.T) {
 		Uint     uint          `config:"uint,required"`
 		Float    float64       `config:"float,required"`
 		Duration time.Duration `config:"duration,required"`
+		Time     time.Time     `config:"time,required"`
+		TimePtr  *time.Time    `config:"time_ptr,required"`
 	}
 
 	var cfg config
@@ -140,6 +177,8 @@ func TestWithAnotherBackend(t *testing.T) {
 		"uint":     "42",
 		"float":    "42.42",
 		"duration": "1ns",
+		"time":     "2001-01-01T01:01:01Z",
+		"time_ptr": "2001-01-01T01:01:01Z",
 	}
 
 	flags := flag.NewFlagSet("test", flag.ContinueOnError)
@@ -153,6 +192,8 @@ func TestWithAnotherBackend(t *testing.T) {
 	require.EqualValues(t, 42, cfg.Uint)
 	require.Equal(t, 99.5, cfg.Float)
 	require.Equal(t, time.Duration(1), cfg.Duration)
+	require.Equal(t, time.Date(2001, 1, 1, 1, 1, 1, 0, time.UTC), cfg.Time)
+	require.Equal(t, time.Date(2001, 1, 1, 1, 1, 1, 0, time.UTC), *cfg.TimePtr)
 }
 
 type store map[string]string
